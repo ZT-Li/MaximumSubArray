f82eeab57962a10f1c760d7e44621c7787b13129
Zhuotang Li <zt.li.fisher@gmail.com>
2020-01-28 15:20:27 -0800

    all java code for maxSubArray

---
+++ Plan.java
@@ -0,0 +1,34 @@
+package cs146F19.Li.Project2;
+
+public class Plan {
+	private int sum;
+	private int arrival;
+	private int departure;
+
+	public Plan(int sum, int arrival, int departure) {
+		this.sum = sum;
+		this.arrival = arrival;
+		this.departure = departure;
+	}
+
+	public void set(int sum, int arrival, int departure) {
+		this.sum = sum;
+		this.arrival = arrival;
+		this.departure = departure;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+	
+	public boolean equals(Plan p) {
+		if(this.sum==p.sum && this.arrival==p.arrival && this.departure==p.departure)
+			return true;
+		else
+			return false;
+	}
+
+	public String toString() {
+		return "MaxSum: " + sum + " Arrival: " + arrival + " Departure: " + departure;
+	}
+}
